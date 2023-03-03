6b837c16f976ff7326fbeef9f996930536a1aa00
Youngho <cyh6099@gmail.com>
2023-03-03 11:13:14 +0900

    feat : 사용자의 이름에 all이 올 수 없는 기능 구현

--- src/main/java/domain/User.java
+++ src/main/java/domain/User.java
@@ -7,13 +7,26 @@ public class User {
     private static final int USER_NAME_MAX_LENGTH = 5;
     private static final String INVALID_NAME_LENGTH_MESSAGE = "이름은 1~5 글자만 가능합니다.";
     private static final String INVALID_NAME_BLANK_MESSAGE = "이름은 공백으로만 이루어지면 안됩니다.";
+
+    private static final String INVALID_NAME = "all";
+    private static final String INVALID_NAME_MESSAGE = "이름은 all일 수 없습니다.";
     private final String name;
 
     public User(final String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(final String name) {
         validateNameLength(name);
         validateBlankName(name);
+        validateNameIsAll(name);
+    }
 
-        this.name = name;
+    private void validateNameIsAll(final String name) {
+        if (name.equals(INVALID_NAME)) {
+            throw new IllegalArgumentException(INVALID_NAME_MESSAGE);
+        }
     }
 
     private void validateNameLength(final String name) {
@@ -27,6 +40,7 @@ public class User {
             throw new IllegalArgumentException(INVALID_NAME_BLANK_MESSAGE);
         }
     }
+
 
     public String getName() {
         return name;
